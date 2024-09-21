26fa1b6a5d2c1cd8286fba334b435a81e2c5eef5
Chenlei Hu <chenlei.hu@mail.utoronto.ca>
2024-09-21 18:19:36 +0900

    Move workflow dropdown to sidebar tab (#893)
    * Initial move to sidebar
    
    Remove broken CSS
    
    Move action buttons
    
    Migrate open workflows
    
    Add basic browse
    
    WIP
    
    Add insert support
    
    Remove legacy workflow manager
    
    Remove unused CSS
    
    Reorder
    
    Remove legacy workflow UI
    
    nit
    
    * Support bookmark
    
    Add workflow bookmark store
    
    nit
    
    Add back bookmark functionality
    
    Correctly load bookmarks
    
    nit
    
    Fix many other issues
    
    Fix this binding
    
    style divider
    
    * Extract tree leaf component
    
    * Hide bookmark section when no bookmarks
    
    * nit
    
    * Fix save
    
    * Add workflows searchbox
    
    * Add search support
    
    * Show total opened
    
    * Add basic test
    
    * Add more tests
    
    * Fix redo/undo test
    
    * Temporarily disable browser tab title test

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1725,12 +1725,6 @@ export class ComfyApp {
           }
         })
     )
-
-    try {
-      this.menu.workflows.registerExtension(this)
-    } catch (error) {
-      console.error(error)
-    }
   }
 
   async #migrateSettings() {
